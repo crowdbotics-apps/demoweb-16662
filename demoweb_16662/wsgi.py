2bbb6d5b92150cbf5c7ef7a0d375735c55efcbe6
Crowdbotics <team@crowdbotics.com>
2020-05-06 10:34:08 +0000

    Update models

--- demoweb_16662/wsgi.py
+++ demoweb_16662/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demoweb_16662.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demoweb_16662.settings")
 
 application = get_wsgi_application()
